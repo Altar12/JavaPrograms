509e6c4c438e1efcc2d086a8b9af2a85eab83660
Altar12 <shubham.singh12500@gmail.com>
2022-06-27 18:36:34 +0530

    first commit

---
+++ src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int num1, num2;
+        num1 = 290;
+        num2 = 543;
+        System.out.println("Sum of " + num1 + " and " + num2 + " is: " + (num1 + num2));
+    }
+}
